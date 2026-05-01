14a632c3fce93ed3a2b71285f81e93532eea1de5
Gleicon Moraes <gleicon@gmail.com>
2026-05-01 18:49:36 -0300

    chore(01-01): initialize go module with didasy/tldr dependency
    - Run go mod init github.com/gleicon/tldt
    - Fetch github.com/didasy/tldr v0.7.0 and go mod tidy
    - Add //go:build ignore to all src/ legacy files to exclude from module (Rule 3: blocked go mod tidy)
    - go.sum generated with 27 checksum entries

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/gleicon/tldt
+
+go 1.26.2
+
+require github.com/didasy/tldr v0.7.0
+
+require github.com/alixaxel/pagerank v0.0.0-20160306110729-14bfb4c1d88c // indirect
--- src/utils.go
+++ src/utils.go
@@ -1,3 +1,5 @@
+//go:build ignore
+
 // Copyright 2015 resumator authors.  All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
